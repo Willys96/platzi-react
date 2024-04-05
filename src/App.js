778510c2c724ee7941b861b3a718111191c019ff
Wilinton Baquero Mora <wbaquero@mihhgs.com>
2024-04-05 07:05:38 -0500

    Commit TODOS => componentes, props, estilos, eventos - state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
 import logo from './platzi.webp';
-import './App.css';
+//import './App.css';
+import React from 'react';
+import { TodoCounter } from "./TodoCounter"
+import { TodoSearch } from "./TodoSearch"
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos = [
+	{ text: 'Corta cebolla', completed: true },
+	{ text: 'Tomar el curso de Intro a React.js', completed: false },
+	{ text: 'Llorar con la Llorona', completed: false },
+	{ text: 'LALALALALA', completed: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//Estados React
+	const [ todos, setTodos ] = React.useState( defaultTodos );
+	const [ searchValue, setSearchValue ] = React.useState( '' );
+	
+
+	//Estados derivados
+	const completedTodos = todos.filter( todo => !!todo.completed).length; // !!todo.completed (doble negación) => obliga a retornar un valor booleano independientemente del tipo de variable que es todo.completed. TRUE siempre que sea diferente de undefined, null, o false 
+	const totalTodos = todos.length;
+	const searchedTodos = todos.filter( 
+		( todo ) => {
+			return todo.text.toLowerCase().includes( searchValue.toLowerCase() );
+		}
+
+		// Codigo identico al anterior
+		// ( todo ) => (
+		// 	todo.text.toLowerCase().includes( searchValue.toLowerCase() )
+		// )		
+	);
+
+	const completeTodo = ( text ) => {
+		const newTodos = [ ...todos ];
+		const todoIndex = newTodos.findIndex( 
+			todo => todo.text == text 
+		);
+		newTodos[ todoIndex ].completed = true; 
+		setTodos( newTodos );	
+	};
+
+	const eliminarTodo = ( text ) => {
+		const newTodos = [ ...todos ];
+		// const todosActulizados = newTodos.filter( todo => {
+		// 	return todo.text != text;
+		// })
+		//setTodos( todosActulizados );
+
+		// Método anterior funciona igual de correcto
+		const todoIndex = newTodos.findIndex( 
+			todo => todo.text == text 
+		); 
+		newTodos.splice( todoIndex, 1 );
+		setTodos( newTodos );
+	}
+	
+
+	return (
+		// <div className="App">
+		//<React.Fragment>
+
+		<>
+			<TodoCounter 
+				completed={ completedTodos } 
+				total={ totalTodos } 
+			/>
+			<TodoSearch
+				searchValue={ searchValue }
+				setSearchValue={ setSearchValue }
+			/>
+
+			<TodoList>
+				{
+					// defaultTodos						
+					searchedTodos
+						.map( 
+							todo => (
+								<TodoItem 
+									key={ todo.text } 
+									text={ todo.text } 
+									completed={ todo.completed } 
+									onComplete={ () => completeTodo( todo.text )  }
+									eliminarTodo={ () => eliminarTodo( todo.text ) }
+								/>
+							)
+						)
+				}
+				{/* {[<TodoCounter/>, <TodoList/>]} */}
+			</TodoList>
+
+			<CreateTodoButton />
+		</>
+
+		//</React.Fragment>
+		// </div>
+	);
 }
+
 
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,25 @@
+import './TodoItem.css'
+
+function TodoItem({ text, completed, onComplete, eliminarTodo }) {
+
+    /*
+    - El ${} se usa para insertar comandos javascript (Validaciones - Condiciones)
+    - El ${...completed && xxx...} indica que se ejecuta si y solo si completed es true, es decir > 0, Cadena es decir diferente a null o undefined o false
+    */   
+
+    return (
+      <li className='TodoItem'>
+        <span 
+            className={ `Icon Icon-check ${ completed && "Icon-check--active" }`}
+            onClick={ onComplete }
+        >V</span>
+        <p className={ `TodoItem-p ${ completed &&  "TodoItem-p--complete" }` }>{ text }</p>
+        <span 
+            className='Icon Icon-delete'
+            onClick={ eliminarTodo }
+        >X</span>
+      </li>
+    );
+  }
+  
+export { TodoItem };
